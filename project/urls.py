9e458ed91f5fda4cb00aafff71c6fdeb1604f084
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2024-12-23 11:46:12 -0300

    Primeira URL e function based view + HttpRequest e HttpResponse + Status Codes

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTPP Resquest <-> HTTP Response
+# MTV (MVC)
+
+
+def my_view(request):
+    print('Posso fazer outras coisas aqui')
+    return HttpResponse('Uma mensagem para alguém especial')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
